388906361a062ec9c2df18d7fd70329ef6eb990c
DeathN7 <vannhu2301@gmail.com>
2024-04-03 20:37:54 +0700

    3/4

---
+++ 59_excel sheet column number/59.js
@@ -0,0 +1,7 @@
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++) {
+        result = result * 26 + (columnTitle.charCodeAt(i) - 64);
+    }
+    return result;
+};
